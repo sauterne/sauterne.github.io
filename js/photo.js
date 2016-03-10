ba24a5e6d7bc80203130ca2961df0b0f479f808c
mo yu <mo yu>
2016-03-10 15:14:34 +0800

    Site updated: 2016-03-10 15:14:33

--- js/photo.js
+++ js/photo.js
@@ -16,8 +16,8 @@ return {
         var html, li = "";
         for (var i = begin; i < end && i < data.length; i++) {
             li += '<li><div class="img-box">' +
-                '<a class="img-bg" rel="example_group" href="https://github.com/sauterne/sauterne.github.io/tree/master/photos/' + data[i] + '?raw=true"></a>' +
-                '<img lazy-src="https://github.com/sauterne/sauterne.github.io/tree/master/photos/' + data[i] + '?raw=true" />' +
+                '<a class="img-bg" rel="example_group" href="https://github.com/sauterne/blog/tree/master/photos/' + data[i] + '?raw=true"></a>' +
+                '<img lazy-src="https://github.com/sauterne/blog/tree/master/photos/' + data[i] + '?raw=true" />' +
                 '</li>';
         }
         $(".img-box-ul").append(li);
